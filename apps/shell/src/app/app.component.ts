1ad67bcb3b6434d6b3639c46c865576ed85b890b
JurgenLubonja <jurgen.lubonja@eng.it>
2023-09-28 12:32:19 +0200

    Renamed Shell Application

---
+++ apps/shell/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-aio-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  // HOST MAIN APPLICATION, everything is run through here
+  // Practically this is SHell Component
+  title = 'host';
+}
