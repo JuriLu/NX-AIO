16ad7cc29424a9c7681f44bb7f927ffe05ee6699
JurgenLubonja <jurgen.lubonja@eng.it>
2023-10-19 11:46:23 +0200

    Working with PDF app

--- libs/workenv-pdf-app/src/lib/pdf-app/pdf-app.component.ts
+++ libs/workenv-pdf-app/src/lib/pdf-app/pdf-app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {GcPdfViewer} from "@grapecity/gcpdfviewer";
+
 
 @Component({
   selector: 'nx-aio-pdf-app',
   templateUrl: './pdf-app.component.html',
   styleUrls: ['./pdf-app.component.scss']
 })
-export class PDFAppComponent {
-
+export class PDFAppComponent implements AfterViewInit{
+  ngAfterViewInit(){
+    const viewer = new GcPdfViewer("#viewer", {
+      workerSrc: "//node_modules/@grapecity/gcpdfviewer/gcpdfviewer.worker.js",
+      restoreViewStateOnLoad: false
+    });
+    viewer.addDefaultPanels();
+    viewer.open('../assets/PDFApp/Introduzione_a_Wave.pdf')
+  }
 }
